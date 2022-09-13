69e8c837d537c3654f46bfc19daf788f2fc8047f
Pallavic5 <pchedge235@gmail.com>
2022-09-13 17:18:44 +0530

    first commit

---
+++ src/com/bridgelabz/snakeladder/SnakeLadder.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.snakeladder;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		
+		System.out.println("WELCOME TO SNAKE AND LADDER PROBLEM");
+
+	}
+
+}
