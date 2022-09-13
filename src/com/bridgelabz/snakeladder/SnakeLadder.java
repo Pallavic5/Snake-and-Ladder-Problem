7cd2c07b56f003fdaab55d119ceb2736738b3c72
Pallavic5 <pchedge235@gmail.com>
2022-09-13 17:56:32 +0530

    Added: Player Checks for options no play, ladder or snake

--- src/com/bridgelabz/snakeladder/SnakeLadder.java
+++ src/com/bridgelabz/snakeladder/SnakeLadder.java
@@ -4,18 +4,38 @@ public class SnakeLadder {
 
 	public static void main(String[] args) {
 		
-		System.out.println("WELCOME TO SNAKE AND LADDER PROBLEM");
+		final int NO_PLAY = 0;
+		final int LADDER = 1;
+		final int SNAKE = 2;
 		
 		int playerStartPosition = 0;
 		System.out.println("Player Position = "+ playerStartPosition);
 		
-		System.out.println("Player rolls the die and get a number: " + dieThrow());
-
-	}
-	
-	public static int dieThrow()
-		{
-	    	int num = (int)(Math.random()* 6) + 1; 
-	    	return num;
+		int playerRollDice = (int)(Math.random()* 6) + 1;
+		System.out.println("Player get a dice number: " + playerRollDice);	
+		
+		int optionCheck = (int) (Math.random() * 100) % 3;
+		System.out.println("Optioncheck: " + optionCheck);
+		
+		if(optionCheck == LADDER ) {
+			
+			System.out.println("Ladder");
+			playerStartPosition = playerStartPosition + playerRollDice  ;
+			System.out.println("Player moves ahead by the: "+ playerStartPosition);
 		}
+		else if(optionCheck == SNAKE) {
+			
+			System.out.println("Snake");
+			playerStartPosition = playerStartPosition - playerRollDice ;
+			System.out.println("Player moves behind by the: "+ playerStartPosition);
+		}
+		else {
+			
+			  System.out.println("No Play,the Player stays in the same position");	  
+		}	
+		
+		if(playerStartPosition < 0)
+			playerStartPosition = 0;
+		System.out.println("PlayerPosition: " + playerStartPosition);
+	}
 }
