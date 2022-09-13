3c7d7debd4f5dec0dc0e058163df2d3ce2dae937
Pallavic5 <pchedge235@gmail.com>
2022-09-13 17:22:14 +0530

    Added: Player's Start Position

--- src/com/bridgelabz/snakeladder/SnakeLadder.java
+++ src/com/bridgelabz/snakeladder/SnakeLadder.java
@@ -5,6 +5,9 @@ public class SnakeLadder {
 	public static void main(String[] args) {
 		
 		System.out.println("WELCOME TO SNAKE AND LADDER PROBLEM");
+		
+		int playerStartPosition = 0;
+		System.out.println("Player Position = "+ playerStartPosition);
 
 	}
 
