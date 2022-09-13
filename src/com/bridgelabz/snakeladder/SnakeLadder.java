37a5611ae1f3330fb8b1c69344de2aa83b3cc12a
Pallavic5 <pchedge235@gmail.com>
2022-09-13 18:03:32 +0530

    Added: Repeat till the Player reaches the winning position 100.

--- src/com/bridgelabz/snakeladder/SnakeLadder.java
+++ src/com/bridgelabz/snakeladder/SnakeLadder.java
@@ -9,33 +9,37 @@ public class SnakeLadder {
 		final int SNAKE = 2;
 		
 		int playerStartPosition = 0;
-		System.out.println("Player Position = "+ playerStartPosition);
 		
-		int playerRollDice = (int)(Math.random()* 6) + 1;
-		System.out.println("Player get a dice number: " + playerRollDice);	
-		
-		int optionCheck = (int) (Math.random() * 100) % 3;
-		System.out.println("Optioncheck: " + optionCheck);
-		
-		if(optionCheck == LADDER ) {
+		while(playerStartPosition < 100) {
 			
-			System.out.println("Ladder");
-			playerStartPosition = playerStartPosition + playerRollDice  ;
-			System.out.println("Player moves ahead by the: "+ playerStartPosition);
+			int playerRollDice = (int)(Math.random() * 6) + 1;
+			System.out.println("Player get a dice number: " + playerRollDice);
+			
+			int optionCheck = (int) (Math.random() * 100) % 3;
+			System.out.println("Optioncheck: " + optionCheck);
+			
+			if((optionCheck == LADDER) && (playerStartPosition + playerRollDice) <= 100){
+				
+				System.out.println("Ladder");
+				playerStartPosition = playerStartPosition + playerRollDice  ;
+			}
+			
+			else if(optionCheck == SNAKE) {
+				
+				System.out.println("Snake");
+				playerStartPosition = playerStartPosition - playerRollDice  ;
+				
+			}
+			
+			else {
+				
+				System.out.println("No Play,the Player stays in the same position");	
+			}
+			
+			if(playerStartPosition < 0)
+				playerStartPosition = 0;
+			System.out.println("PlayerPosition: " + playerStartPosition);
+			
 		}
-		else if(optionCheck == SNAKE) {
-			
-			System.out.println("Snake");
-			playerStartPosition = playerStartPosition - playerRollDice ;
-			System.out.println("Player moves behind by the: "+ playerStartPosition);
-		}
-		else {
-			
-			  System.out.println("No Play,the Player stays in the same position");	  
-		}	
-		
-		if(playerStartPosition < 0)
-			playerStartPosition = 0;
-		System.out.println("PlayerPosition: " + playerStartPosition);
 	}
 }
